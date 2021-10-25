3039eecbaa1d08868043c682bf33655a2f9bc181
p22rlugo <p22rlugo@fairfieldprep.org>
2021-10-25 10:15:07 -0400

    Add file

---
+++ lab_3-1.py
@@ -0,0 +1,13 @@
+# Ryan Lugo: RJL 10/25/21
+
+import time
+import math
+
+t1 = time.perf_counter()
+
+2 **2
+# math.pow(2,2)
+
+t2 = time.perf_counter()
+timeprocessed = t2 - t1
+print(timeprocessed)
