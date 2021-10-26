ab762b10b6c5e01a004ba02dda40eceed689bd29
p22rlugo <p22rlugo@fairfieldprep.org>
2021-10-26 10:14:44 -0400

    Add file

---
+++ lab_4-1.py
@@ -0,0 +1,10 @@
+# Ryan Lugo: RJL 10/26/21
+
+statement = "I'm loving this short story I'm reading, \"The Fall of the House of Usher\""
+word = "apple"
+
+print("Fairfield" + " Prep")
+print("-" * 20)
+print(statement)
+print(len(statement))
+print(word[3])
