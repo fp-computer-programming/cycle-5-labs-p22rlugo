88a0d57004289fee4fab522b76f84425f80028e1
p22rlugo <p22rlugo@fairfieldprep.org>
2021-10-27 10:15:58 -0400

    Add file

--- lab_5-2.py
+++ lab_5-2.py
@@ -4,10 +4,10 @@ first_string = input("First Word?: ")
 second_string = input("Second Word?: ")
 
 if first_string > second_string:
-    print(first_string + " is bigger than " + second_string + " in length.")
+    print(first_string + " is bigger than " + second_string)
 elif first_string < second_string:
-    print(second_string + " is bigger than " + first_string + " in length.")
+    print(second_string + " is bigger than " + first_string)
 elif first_string == second_string:
-    print(first_string + " is equal to " + second_string + " in length.")
+    print(first_string + " is equal to " + second_string)
 else:
-    print("Error.")
+    print("Error")
