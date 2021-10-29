36e9eaba78e233313426c3f2ad4597663b8edd62
p22rlugo <p22rlugo@fairfieldprep.org>
2021-10-29 10:48:22 -0400

    Add file

---
+++ lab_6-1.py
@@ -0,0 +1,11 @@
+# Ryan Lugo: RJL 10/29/21
+
+word = "flibbertigibbet"
+find_word = word.find("t")
+
+print(find_word)
+
+string = "I wish, I wish, I was a fish."
+str_split = string.split(sep=",")
+
+print(str_split)
---
+++ lab_6-2.py
@@ -0,0 +1,7 @@
+# Ryan Lugo: RJL 10/29/21
+
+name_one = input("What's player's One name?: ")
+name_two = input("What's player's Two name?: ")
+
+formatter = "Hello, {0}. My name is {1}.".format(name_one,name_two)
+print(formatter)
